39e1958b91d64cbfd5eaba84cd6f8c207165efb9
tuna1402 <tuna1402@gmail.com>
2024-11-27 12:26:40 +0900

    work

--- src/main/java/com/ll/polynomialCal.java
+++ src/main/java/com/ll/polynomialCal.java
@@ -3,20 +3,20 @@ package com.ll;
 import java.util.Arrays;
 
 public class polynomialCal {
-    public int sum(int num1, int num2) {
+    public double sum(double num1, double num2) {
         return num1 + num2;
     }
 
-    public int sub(int num1, int num2) {
+    public double sub(double num1, double num2) {
         return num1 - num2;
     }
 
-    public int mul(int num1, int num2) {
+    public double mul(double num1, double num2) {
         return num1 * num2;
     }
 
-    public double div(int num1, int num2) {
-        return (double)num1 / num2;
+    public double div(double num1, double num2) {
+        return num1 / num2;
     }
 
     public String polyCal(String expression) {
@@ -34,5 +34,39 @@ public class polynomialCal {
         } else {
             return expression;
         }
+    }
+
+    public int stringCal(String innerExpression) {
+        String[] arr = innerExpression.split("");
+        switch (arr[2]) {
+            case "+" -> {
+                int result = Integer.parseInt(arr[0]) + Integer.parseInt(arr[4]);
+                return result;
+            }
+            case "-" -> {
+                int result = Integer.parseInt(arr[0]) - Integer.parseInt(arr[4]);
+                return result;
+            }
+            case "*" -> {
+                int result = Integer.parseInt(arr[0]) * Integer.parseInt(arr[4]);
+                return result;
+            }
+            case "/" -> {
+                int result = Integer.parseInt(arr[0]) / Integer.parseInt(arr[4]);
+                return result;
+            }
+        }
+        return 0;
+    }
+
+    public int newCal(String expression) {
+        String[] arr = expression.split("");
+        String exp1 = polyCal(expression);
+        int answer1 = stringCal(exp1);
+        String exp2 = Integer.toString(answer1);
+        String exp3 = exp2 + ' ' + arr[8] + ' ' + arr[10];
+        System.out.println(exp3);
+        int result = stringCal(exp3);
+        return result;
     }
 }
--- src/test/java/com/ll/TddCalTest.java
+++ src/test/java/com/ll/TddCalTest.java
@@ -8,21 +8,21 @@ public class TddCalTest {
     @Test
     public void addTest() {
         polynomialCal calculator = new polynomialCal();
-        int popoResult = calculator.sum(1, 2);
-        int poneResult = calculator.sum(-10, 5);
-        int neneResult = calculator.sum(-3, -7);
+        double popoResult = calculator.sum(1, 2);
+        double poneResult = calculator.sum(-10, 5);
+        double neneResult = calculator.sum(-3, -7);
 
-        assertThat(popoResult).isEqualTo(3);
-        assertThat(poneResult).isEqualTo(-5);
-        assertThat(neneResult).isEqualTo(-10);
+        assertThat(popoResult).isEqualTo(3.0);
+        assertThat(poneResult).isEqualTo(-5.0);
+        assertThat(neneResult).isEqualTo(-10.0);
     }
 
     @Test
     public void subTest() {
         polynomialCal calculator = new polynomialCal();
-        int popoResult = calculator.sub(1, 2);
-        int poneResult = calculator.sub(-10, 5);
-        int neneResult = calculator.sub(-3, -7);
+        double popoResult = calculator.sub(1, 2);
+        double poneResult = calculator.sub(-10, 5);
+        double neneResult = calculator.sub(-3, -7);
 
         assertThat(popoResult).isEqualTo(-1);
         assertThat(poneResult).isEqualTo(-15);
@@ -32,9 +32,9 @@ public class TddCalTest {
     @Test
     public void mulTest() {
         polynomialCal calculator = new polynomialCal();
-        int popoResult = calculator.mul(1, 2);
-        int poneResult = calculator.mul(-10, 5);
-        int neneResult = calculator.mul(-3, -7);
+        double popoResult = calculator.mul(1, 2);
+        double poneResult = calculator.mul(-10, 5);
+        double neneResult = calculator.mul(-3, -7);
 
         assertThat(popoResult).isEqualTo(2);
         assertThat(poneResult).isEqualTo(-50);
@@ -54,11 +54,27 @@ public class TddCalTest {
     }
 
     @Test
-    public void combineTest() {
+    public void combineTest1() {
         polynomialCal calculator = new polynomialCal();
         String combnineResult = calculator.polyCal("(3 + 5)");
 
         assertThat(combnineResult).isEqualTo("3 + 5");
 
+    }
+
+    @Test
+    public void stringcalTest1() {
+        polynomialCal calculator = new polynomialCal();
+        int stringCalResult = calculator.stringCal("3 + 5");
+
+        assertThat(stringCalResult).isEqualTo(8);
+    }
+
+    @Test
+    public void combineTest2() {
+        polynomialCal calculator = new polynomialCal();
+        int stringCalResult = calculator.newCal("(3 + 5) * 5");
+
+        assertThat(stringCalResult).isEqualTo(40);
     }
 }
